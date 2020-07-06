55048f39f0f39df585926148685d7086dbc5cf4a
Ger Hobbelt <ger@hobbelt.com>
2020-07-06 04:54:39 +0200

    - refactored the tests for easier diagnosis - do not load .gitignore files in these tests, but use different filenames instead, so the test rig doesn't interfere with the project's git processes ever.

--- test/index.js
+++ test/index.js
@@ -9,7 +9,7 @@ var FIXTURE = fs.readFileSync(__dirname + '/./.gitignore-fixture', 'utf8');
 var NO_NEGATIVES_FIXTURE = fs.readFileSync(__dirname + '/./.gitignore-no-negatives', 'utf8');
 
 describe('gitignore parser', function() {
-  xdescribe('parse()', function() {
+  describe('parse()', function() {
     it('should parse some content', function() {
       var parsed = LIB.parse(FIXTURE);
       assert.strictEqual(parsed.length, 2);
@@ -128,139 +128,170 @@ describe('gitignore parser', function() {
   });
 
   describe('compile()', function() {
+    let ignore, gitignoreNoNegatives;
+
     beforeEach(function() {
-      this.gitignore = LIB.compile(FIXTURE);
-      this.gitignoreNoNegatives = LIB.compile(NO_NEGATIVES_FIXTURE);
+      gitignore = LIB.compile(FIXTURE);
+      gitignoreNoNegatives = LIB.compile(NO_NEGATIVES_FIXTURE);
     });
+
+    function gitignoreAccepts(str, expects) {
+        assert.strictEqual(gitignore.accepts(str, expects), expects, `expected '${str}' to be ${ expects ? "accepted" : "rejected" }`);
+    }
+    function gitignoreNoNegativesAccepts(str, expects) {
+        assert.strictEqual(gitignore.accepts(str, expects), expects, `expected '${str}' to be ${ expects ? "accepted" : "rejected" } when we use gitignoreNoNegatives`);
+    }
 
     describe('accepts()', function() {
       it('should accept the given filenames', function() {
-        assert.strictEqual(this.gitignore.accepts('test/index.js'), true);
-        assert.strictEqual(this.gitignore.accepts('wat/test/index.js'), true);
-        assert.strictEqual(this.gitignoreNoNegatives.accepts('test/index.js'), true);
-        assert.strictEqual(this.gitignoreNoNegatives.accepts('node_modules.json'), true);
+        gitignoreAccepts('test/index.js', true);
+        gitignoreAccepts('wat/test/index.js', true);
+        gitignoreNoNegativesAccepts('test/index.js', true);
+        gitignoreNoNegativesAccepts('node_modules.json', true);
       });
 
       it('should not accept the given filenames', function () {
-        assert.strictEqual(this.gitignore.accepts('test.swp'), false);
-        assert.strictEqual(this.gitignore.accepts('foo/test.swp'), false);
-        assert.strictEqual(this.gitignore.accepts('node_modules/wat.js', false), false, 'node_modules/wat.js');
-        assert.strictEqual(this.gitignore.accepts('foo/bar.wat'), false);
-        assert.strictEqual(this.gitignoreNoNegatives.accepts('node_modules/wat.js'), false);
+        gitignoreAccepts('test.swp', false);
+        gitignoreAccepts('foo/test.swp', false);
+        gitignoreAccepts('node_modules/wat.js', false);
+        gitignoreAccepts('foo/bar.wat', false);
+        gitignoreNoNegativesAccepts('node_modules/wat.js', false);
       });
 
       it('should not accept the given directory', function() {
-        assert.strictEqual(this.gitignore.accepts('nonexistent'), false);
-        assert.strictEqual(this.gitignore.accepts('nonexistent/bar'), false);
-        assert.strictEqual(this.gitignoreNoNegatives.accepts('node_modules'), false);
+        gitignoreAccepts('nonexistent', false);
+        gitignoreAccepts('nonexistent/bar', false);
+        gitignoreNoNegativesAccepts('node_modules', false);
       });
 
       it('should accept unignored files in ignored directories', function() {
-        assert.strictEqual(this.gitignore.accepts('nonexistent/foo'), true);
+        gitignoreAccepts('nonexistent/foo', true);
       });
 
       it('should accept nested unignored files in ignored directories', function() {
-        assert.strictEqual(this.gitignore.accepts('nonexistent/foo/wat'), true);
+        gitignoreAccepts('nonexistent/foo/wat', true);
       });
     });
+
+    function gitignoreDenies(str, expects) {
+        assert.strictEqual(gitignore.denies(str, expects), expects, `expected '${str}' to be ${ expects ? "denied" : "accepted" }`);
+    }
+    function gitignoreNoNegativesDenies(str, expects) {
+        assert.strictEqual(gitignore.denies(str, expects), expects, `expected '${str}' to be ${ expects ? "denied" : "accepted" } when we use gitignoreNoNegatives`);
+    }
 
     describe('denies()', function () {
       it('should deny the given filenames', function () {
-        assert.strictEqual(this.gitignore.denies('test.swp'), true);
-        assert.strictEqual(this.gitignore.denies('foo/test.swp'), true);
-        assert.strictEqual(this.gitignore.denies('node_modules/wat.js'), true);
-        assert.strictEqual(this.gitignore.denies('foo/bar.wat'), true);
-        assert.strictEqual(this.gitignoreNoNegatives.denies('node_modules/wat.js'), true);
+        gitignoreDenies('test.swp', true);
+        gitignoreDenies('foo/test.swp', true);
+        gitignoreDenies('node_modules/wat.js', true);
+        gitignoreDenies('foo/bar.wat', true);
+        gitignoreNoNegativesDenies('node_modules/wat.js', true);
       });
 
       it('should not deny the given filenames', function() {
-        assert.strictEqual(this.gitignore.denies('test/index.js'), false);
-        assert.strictEqual(this.gitignore.denies('wat/test/index.js'), false);
-        assert.strictEqual(this.gitignoreNoNegatives.denies('test/index.js'), false);
-        assert.strictEqual(this.gitignoreNoNegatives.denies('wat/test/index.js'), false);
-        assert.strictEqual(this.gitignoreNoNegatives.denies('node_modules.json'), false);
+        gitignoreDenies('test/index.js', false);
+        gitignoreDenies('wat/test/index.js', false);
+        gitignoreNoNegativesDenies('test/index.js', false);
+        gitignoreNoNegativesDenies('wat/test/index.js', false);
+        gitignoreNoNegativesDenies('node_modules.json', false);
       });
 
       it('should deny the given directory', function() {
-        assert.strictEqual(this.gitignore.denies('nonexistent'), true);
-        assert.strictEqual(this.gitignore.denies('nonexistent/bar'), true);
-        assert.strictEqual(this.gitignoreNoNegatives.denies('node_modules'), true);
-        assert.strictEqual(this.gitignoreNoNegatives.denies('node_modules/foo'), true);
+        gitignoreDenies('nonexistent', true);
+        gitignoreDenies('nonexistent/bar', true);
+        gitignoreNoNegativesDenies('node_modules', true);
+        gitignoreNoNegativesDenies('node_modules/foo', true);
       });
 
       it('should not deny unignored files in ignored directories', function() {
-        assert.strictEqual(this.gitignore.denies('nonexistent/foo'), false);
+        gitignoreDenies('nonexistent/foo', false);
       });
 
       it('should not deny nested unignored files in ignored directories', function() {
-        assert.strictEqual(this.gitignore.denies('nonexistent/foo/wat'), false);
+        gitignoreDenies('nonexistent/foo/wat', false);
       });
     });
 
+    function gitignoreMaybeOkays(str, expects) {
+        assert.strictEqual(gitignore.maybe(str, expects), expects, `expected '${str}' to maybe be ${ expects ? "rejected" : "accepted" }`);
+    }
+    function gitignoreNoNegativesMaybeOkays(str, expects) {
+        assert.strictEqual(gitignore.maybe(str, expects), expects, `expected '${str}' to maybe be ${ expects ? "rejected" : "accepted" } when we use gitignoreNoNegatives`);
+    }
+
     describe('maybe()', function() {
       it('should return true for directories not mentioned by .gitignore', function() {
-        assert.strictEqual(this.gitignore.maybe('lib'), true);
-        assert.strictEqual(this.gitignore.maybe('lib/foo/bar'), true);
-        assert.strictEqual(this.gitignoreNoNegatives.maybe('lib'), true);
-        assert.strictEqual(this.gitignoreNoNegatives.maybe('lib/foo/bar'), true);
+        gitignoreMaybeOkays('lib', true);
+        gitignoreMaybeOkays('lib/foo/bar', true);
+        gitignoreNoNegativesMaybeOkays('lib', true);
+        gitignoreNoNegativesMaybeOkays('lib/foo/bar', true);
       });
 
       it('should return false for directories explicitly mentioned by .gitignore', function() {
-        assert.strictEqual(this.gitignore.maybe('baz'), false);
-        assert.strictEqual(this.gitignore.maybe('baz/wat/foo'), false);
-        assert.strictEqual(this.gitignoreNoNegatives.maybe('node_modules'), false);
+        gitignoreMaybeOkays('baz', false);
+        gitignoreMaybeOkays('baz/wat/foo', false);
+        gitignoreNoNegativesMaybeOkays('node_modules', false);
       });
 
       it('should return true for ignored directories that have exceptions', function() {
-        assert.strictEqual(this.gitignore.maybe('nonexistent'), true);
-        assert.strictEqual(this.gitignore.maybe('nonexistent/foo'), true);
-        assert.strictEqual(this.gitignore.maybe('nonexistent/foo/bar'), true);
+        gitignoreMaybeOkays('nonexistent', true);
+        gitignoreMaybeOkays('nonexistent/foo', true);
+        gitignoreMaybeOkays('nonexistent/foo/bar', true);
       });
 
       it('should return false for non exceptions of ignored subdirectories', function() {
-        assert.strictEqual(this.gitignore.maybe('nonexistent/wat'), false);
-        assert.strictEqual(this.gitignore.maybe('nonexistent/wat/foo'), false);
-        assert.strictEqual(this.gitignoreNoNegatives.maybe('node_modules/wat/foo'), false);
+        gitignoreMaybeOkays('nonexistent/wat', false);
+        gitignoreMaybeOkays('nonexistent/wat/foo', false);
+        gitignoreNoNegativesMaybeOkays('node_modules/wat/foo', false);
       });
     });
   });
 
-xdescribe('github issues & misc tests', function () {
+describe('github issues & misc tests', function () {
+
+    function gitignoreAccepts(gitignore, str, expects) {
+        assert.strictEqual(gitignore.accepts(str, expects), expects, `expected '${str}' to be ${ expects ? "accepted" : "rejected" }`);
+    }
+    function gitignoreDenies(gitignore, str, expects) {
+        assert.strictEqual(gitignore.denies(str, expects), expects, `expected '${str}' to be ${ expects ? "denied" : "accepted" }`);
+    }
+
   describe('Test case: a', function() {
     it('should only accept a/2/a', function() {
-      const gitignore = LIB.compile(fs.readFileSync(__dirname + '/a/.gitignore', 'utf8'));
-      assert.strictEqual(gitignore.accepts('a/2/a'), true);
-      assert.strictEqual(gitignore.accepts('a/3/a'), false);
+      const gitignore = LIB.compile(fs.readFileSync(__dirname + '/a/.gitignore-fixture', 'utf8'));
+      gitignoreAccepts(gitignore, 'a/2/a', true);
+      gitignoreAccepts(gitignore, 'a/3/a', false);
     });
   })
 
   describe('issue #12', function () {
     it('should not fail test A', function () {
             var gitignore = LIB.compile('/ajax/libs/bPopup/*b*');
-      assert.strictEqual(gitignore.accepts('/ajax/libs/bPopup/0.9.0'), true);  //output false
+      gitignoreAccepts(gitignore, '/ajax/libs/bPopup/0.9.0', true);  //output false
     });
 
     it('should not fail test B', function () {
       var gitignore = LIB.compile('/ajax/libs/jquery-form-validator/2.2');
-assert.strictEqual(gitignore.accepts('/ajax/libs/jquery-form-validator/2.2.43'), true); //output false
+gitignoreAccepts(gitignore, '/ajax/libs/jquery-form-validator/2.2.43', true); //output false
     });
 
     it('should not fail test C', function () {
       var gitignore = LIB.compile('/ajax/libs/punycode/2.0');
-assert.strictEqual(gitignore.accepts('/ajax/libs/punycode/2.0.0'), true); //output false
+gitignoreAccepts(gitignore, '/ajax/libs/punycode/2.0.0', true); //output false
     });
 
     it('should not fail test D', function () {
       var gitignore = LIB.compile('/ajax/libs/typescript/*dev*');
-assert.strictEqual(gitignore.accepts('/ajax/libs/typescript/2.0.6-insiders.20161014'), true); //output false
+gitignoreAccepts(gitignore, '/ajax/libs/typescript/2.0.6-insiders.20161014', true); //output false
     });
   });
 
   describe('issue #14', function () {
     it('should not fail test A', function () {
-            var gitignore = LIB.compile('node_modules');
-      assert.strictEqual(gitignore.denies('packages/my-package/node-modules'), true);
-      assert.strictEqual(gitignore.accepts('packages/my-package/node-modules'), false);
+            var gitignore = LIB.compile('node-modules');
+      gitignoreDenies(gitignore, 'packages/my-package/node-modules', true);
+      gitignoreAccepts(gitignore, 'packages/my-package/node-modules', false);
     });
   });
 });
