9812f58670ea6c74db14332be027800ac3bbdd6c
Guiorgy <Guiorgy123@gmail.com>
2024-11-19 21:46:27 +0400

    refactored duplicate Regex patterns into constants

--- RegexPatterns.cs
+++ RegexPatterns.cs
@@ -8,7 +8,57 @@ internal static
 #endif
     class RegexPatterns
 {
+    //language=Regex
+    private const string MatchEmptyRegexPattern = "$^";
+    //language=Regex
+    private const string RangeRegexPattern = @"^((?:[^\[\\]|(?:\\.))*)\[((?:[^\]\\]|(?:\\.))*)\]";
+    //language=Regex
+    private const string BackslashRegexPattern = @"\\(.)";
+    //language=Regex
+    private const string SpecialCharactersRegexPattern = @"[\-\[\]\{\}\(\)\+\.\\\^\$\|]";
+    //language=Regex
+    private const string QuestionMarkRegexPattern = @"\?";
+    //language=Regex
+    private const string SlashDoubleAsteriksSlashRegexPattern = @"\/\*\*\/";
+    //language=Regex
+    private const string DoubleAsteriksSlashRegexPattern = @"^\*\*\/";
+    //language=Regex
+    private const string SlashDoubleAsteriksRegexPattern = @"\/\*\*$";
+    //language=Regex
+    private const string DoubleAsteriksRegexPattern = @"\*\*";
+    //language=Regex
+    private const string SlashAsteriksEndOrSlashRegexPattern = @"\/\*(\/|$)";
+    //language=Regex
+    private const string AsteriksRegexPattern = @"\*";
+    //language=Regex
+    private const string SlashRegexPattern = @"\/";
+
 #if NET7_0_OR_GREATER
+    [GeneratedRegex(MatchEmptyRegexPattern)]
+    private static partial Regex GeneratedMatchEmptyRegex();
+    [GeneratedRegex(RangeRegexPattern)]
+    private static partial Regex GeneratedRangeRegex();
+    [GeneratedRegex(BackslashRegexPattern)]
+    private static partial Regex GeneratedBackslashRegex();
+    [GeneratedRegex(SpecialCharactersRegexPattern)]
+    private static partial Regex GeneratedSpecialCharactersRegex();
+    [GeneratedRegex(QuestionMarkRegexPattern)]
+    private static partial Regex GeneratedQuestionMarkRegex();
+    [GeneratedRegex(SlashDoubleAsteriksSlashRegexPattern)]
+    private static partial Regex GeneratedSlashDoubleAsteriksSlashRegex();
+    [GeneratedRegex(DoubleAsteriksSlashRegexPattern)]
+    private static partial Regex GeneratedDoubleAsteriksSlashRegex();
+    [GeneratedRegex(SlashDoubleAsteriksRegexPattern)]
+    private static partial Regex GeneratedSlashDoubleAsteriksRegex();
+    [GeneratedRegex(DoubleAsteriksRegexPattern)]
+    private static partial Regex GeneratedDoubleAsteriksRegex();
+    [GeneratedRegex(SlashAsteriksEndOrSlashRegexPattern)]
+    private static partial Regex GeneratedSlashAsteriksEndOrSlashRegex();
+    [GeneratedRegex(AsteriksRegexPattern)]
+    private static partial Regex GeneratedAsteriksRegex();
+    [GeneratedRegex(SlashRegexPattern)]
+    private static partial Regex GeneratedSlashRegex();
+
     public static readonly Regex MatchEmptyRegex = GeneratedMatchEmptyRegex();
     public static readonly Regex RangeRegex = GeneratedRangeRegex();
     public static readonly Regex BackslashRegex = GeneratedBackslashRegex();
@@ -21,44 +71,18 @@ internal static
     public static readonly Regex SlashAsteriksEndOrSlashRegex = GeneratedSlashAsteriksEndOrSlashRegex();
     public static readonly Regex AsteriksRegex = GeneratedAsteriksRegex();
     public static readonly Regex SlashRegex = GeneratedSlashRegex();
-
-    [GeneratedRegex("$^")]
-    private static partial Regex GeneratedMatchEmptyRegex();
-    [GeneratedRegex(@"^((?:[^\[\\]|(?:\\.))*)\[((?:[^\]\\]|(?:\\.))*)\]")]
-    private static partial Regex GeneratedRangeRegex();
-    [GeneratedRegex(@"\\(.)")]
-    private static partial Regex GeneratedBackslashRegex();
-    [GeneratedRegex(@"[\-\[\]\{\}\(\)\+\.\\\^\$\|]")]
-    private static partial Regex GeneratedSpecialCharactersRegex();
-    [GeneratedRegex(@"\?")]
-    private static partial Regex GeneratedQuestionMarkRegex();
-    [GeneratedRegex(@"\/\*\*\/")]
-    private static partial Regex GeneratedSlashDoubleAsteriksSlashRegex();
-    [GeneratedRegex(@"^\*\*\/")]
-    private static partial Regex GeneratedDoubleAsteriksSlashRegex();
-    [GeneratedRegex(@"\/\*\*$")]
-    private static partial Regex GeneratedSlashDoubleAsteriksRegex();
-    [GeneratedRegex(@"\*\*")]
-    private static partial Regex GeneratedDoubleAsteriksRegex();
-    [GeneratedRegex(@"\/\*(\/|$)")]
-    private static partial Regex GeneratedSlashAsteriksEndOrSlashRegex();
-    [GeneratedRegex(@"\*")]
-    private static partial Regex GeneratedAsteriksRegex();
-    [GeneratedRegex(@"\/")]
-    private static partial Regex GeneratedSlashRegex();
-
 #else
-    public static readonly Regex MatchEmptyRegex = new("$^", RegexOptions.Compiled);
-    public static readonly Regex RangeRegex = new(@"^((?:[^\[\\]|(?:\\.))*)\[((?:[^\]\\]|(?:\\.))*)\]", RegexOptions.Compiled);
-    public static readonly Regex BackslashRegex = new(@"\\(.)", RegexOptions.Compiled);
-    public static readonly Regex SpecialCharactersRegex = new(@"[\-\[\]\{\}\(\)\+\.\\\^\$\|]", RegexOptions.Compiled);
-    public static readonly Regex QuestionMarkRegex = new(@"\?", RegexOptions.Compiled);
-    public static readonly Regex SlashDoubleAsteriksSlashRegex = new(@"\/\*\*\/", RegexOptions.Compiled);
-    public static readonly Regex DoubleAsteriksSlashRegex = new(@"^\*\*\/", RegexOptions.Compiled);
-    public static readonly Regex SlashDoubleAsteriksRegex = new(@"\/\*\*$", RegexOptions.Compiled);
-    public static readonly Regex DoubleAsteriksRegex = new(@"\*\*", RegexOptions.Compiled);
-    public static readonly Regex SlashAsteriksEndOrSlashRegex = new(@"\/\*(\/|$)", RegexOptions.Compiled);
-    public static readonly Regex AsteriksRegex = new(@"\*", RegexOptions.Compiled);
-    public static readonly Regex SlashRegex = new(@"\/", RegexOptions.Compiled);
+    public static readonly Regex MatchEmptyRegex = new(MatchEmptyRegexPattern, RegexOptions.Compiled);
+    public static readonly Regex RangeRegex = new(RangeRegexPattern, RegexOptions.Compiled);
+    public static readonly Regex BackslashRegex = new(BackslashRegexPattern, RegexOptions.Compiled);
+    public static readonly Regex SpecialCharactersRegex = new(SpecialCharactersRegexPattern, RegexOptions.Compiled);
+    public static readonly Regex QuestionMarkRegex = new(QuestionMarkRegexPattern, RegexOptions.Compiled);
+    public static readonly Regex SlashDoubleAsteriksSlashRegex = new(SlashDoubleAsteriksSlashRegexPattern, RegexOptions.Compiled);
+    public static readonly Regex DoubleAsteriksSlashRegex = new(DoubleAsteriksSlashRegexPattern, RegexOptions.Compiled);
+    public static readonly Regex SlashDoubleAsteriksRegex = new(SlashDoubleAsteriksRegexPattern, RegexOptions.Compiled);
+    public static readonly Regex DoubleAsteriksRegex = new(DoubleAsteriksRegexPattern, RegexOptions.Compiled);
+    public static readonly Regex SlashAsteriksEndOrSlashRegex = new(SlashAsteriksEndOrSlashRegexPattern, RegexOptions.Compiled);
+    public static readonly Regex AsteriksRegex = new(AsteriksRegexPattern, RegexOptions.Compiled);
+    public static readonly Regex SlashRegex = new(SlashRegexPattern, RegexOptions.Compiled);
 #endif
 }
